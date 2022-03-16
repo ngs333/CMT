87493fa72c3e7d72ef209ebdf176bc9351520da7
weathon <wguo6358@gmail.com>
2022-03-16 22:50:40 +0000

    to HPC

--- src/apps/HMDistanceTest.h
+++ src/apps/HMDistanceTest.h
@@ -174,7 +174,7 @@ void kNNSearchCompare(const std::string& fileNamePrefix, int k, int n) {
 	for (const auto& [np, nQueries] : nofPoints) {
 		for (const auto& [pivType, pivVal] : pivotTypeMap) {
 			for (const auto& [parType, parVal] : partTypeMap) {
-				for (for mr=k;mr<k+n;mr++) {
+				for (int mr=k;mr<k+n;mr++) {
 					kNNSearchCompare(np, nQueries, pivType, parType, mr, fileNamePrefix );
 				}
 			}
